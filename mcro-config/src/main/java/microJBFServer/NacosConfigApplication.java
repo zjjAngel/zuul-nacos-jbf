1149743e6274cea14513227ae2baa711003b3016
一树一菩提 <7520102+zjj1103@user.noreply.gitee.com>
2020-09-28 15:52:21 +0800

    init first

---
+++ mcro-config/src/main/java/microJBFServer/NacosConfigApplication.java
@@ -0,0 +1,20 @@
+package microJBFServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class NacosConfigApplication
+{
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosConfigApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :" +userName+"; age: "+userAge);
+    }
+}
